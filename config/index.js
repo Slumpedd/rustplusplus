965d74d261025a6e31dbc6771c255bb5d7b0b4be
Slumpedd <73353114+Slumpedd@users.noreply.github.com>
2024-02-23 16:51:44 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'rustplusplus',
-        clientId: process.env.RPP_DISCORD_CLIENT_ID || '',
+        clientId: process.env.RPP_DISCORD_CLIENT_ID || '1210705026314407998',
         token: process.env.RPP_DISCORD_TOKEN || '',
         needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true, /* If true, only admins can delete (server, switch..), manage credentials and reset a channel */
     }
